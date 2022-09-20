85266cf9dfd552a7b260db1de1edf2a817360a02
hohno-46466 <hohno.46466@gmail.com>
2022-09-21 01:39:38 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

---
+++ dpsws2022-2-20220920-2415/pLaTeX/doit-wsl.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+nkf -w main.tex > _main-utf8.tex
+echo
+# echo "Note: 最後にくっついているコメントを削除あるいは直上にある \begin{comment}..\end{comment} の中に入れること"
+# sleep 1
+# vi main-utf8.tex
+
+# function open() {
+#   cmd.exe /c start $(wslpath -w  $(pwd)/$1)
+# }
+
+platex dpsws2022-RaspberryWorkbench.tex && platex dpsws2022-RaspberryWorkbench.tex && platex dpsws2022-RaspberryWorkbench.tex \
+&& dvipdfmx dpsws2022-RaspberryWorkbench.dvi \
+&& cmd.exe /c start $(wslpath -w $(pwd)/dpsws2022-RaspberryWorkbench.pdf)
